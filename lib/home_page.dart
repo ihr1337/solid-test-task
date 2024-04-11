8b277d587ae678822c5e618409afa8a878f5a066
Ihor Kurbanov <ihor.kurbanov@hebronsoft.com>
2024-04-12 00:03:15 +0300

    remove unused variable

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,7 +15,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Color backgroundColor = Colors.white;
-  Color textColor = Colors.black;
 
   void changeBackgroundColor() {
     setState(() {
